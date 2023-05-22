cdfa013750aa1ca95bfe8401d49d71482a0060f2
Stanislav Moskalenko <maskal2719@gmail.com>
2023-05-22 16:10:25 +0300

    add any projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,4 +1,4 @@
-import {socialNetworkImg, todoImg} from "../utils/images";
+import {counterImg, socialNetworkImg, standImg, todoImg} from "../utils/images";
 
 
 type ProjectsDataType = {
@@ -26,30 +26,16 @@ export const projectsData: Array<ProjectsDataType> = [
     },
     {
         projectName: 'Counter',
-        img: socialNetworkImg,
+        img: counterImg,
         linkCode: '',
         linkPreview: '',
         description: 'Counter is a test project which (to tell the truth) doesn\'t have a lot of practical use. It allows to do some settings which influence on the way how the counter works.'
     },
     {
-        projectName: 'Social Network',
-        img: todoImg,
+        projectName: 'Stand',
+        img: standImg,
         linkCode: '',
         linkPreview: '',
         description: 'Social Network is an online platform that allows people to create an account and interact with other people on the website. Users can build there virtual world on the site, make friends and share their thoughts and ideas by writing a post.'
-    },
-    {
-        projectName: 'ToDo List',
-        img: socialNetworkImg,
-        linkCode: '',
-        linkPreview: '',
-        description: 'ToDo List is an online platform for organizing your personal or work tasks which can help you to increase the productivity, prioritise tasks, manage tasks effectively and improve time management.'
-    },
-    {
-        projectName: 'Counter',
-        img: todoImg,
-        linkCode: '',
-        linkPreview: '',
-        description: 'Counter is a test project which (to tell the truth) doesn\'t have a lot of practical use. It allows to do some settings which influence on the way how the counter works.'
-    },
+    }
 ]
--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,10 +1,14 @@
 import todoImg from "../assets/image/todo.jpg"
 import socialNetworkImg from "../assets/image/socialNetwork.jpg"
 import programmerImg from "../assets/image/programmer.png"
+import counterImg from "../assets/image/counter.png"
+import standImg from "../assets/image/stand.jpg"
 
 
 export {
     todoImg,
     socialNetworkImg,
-    programmerImg
+    programmerImg,
+    counterImg,
+    standImg
 }
